37f030516eb3fd0e1706419e8f57d3122490d60a
RAO YANG <lightrao@hotmail.com>
2022-12-09 00:56:06 +0800

    Creating a Document with MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // ./app.js
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoPractice = require("./mongo");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 // store a document，each product would be a document in our collection
-app.post("/products");
+app.post("/products", mongoPractice.createProduct);
 
 // give me all the documents stored in a specific collection
 app.get("/products");
--- mongo.js
+++ mongo.js
@@ -1,22 +1,29 @@
 // ./mongo.js
 // responsible for logic related to the connection to MongoDB
 require("dotenv").config();
-const MongoClient = require("mongodb").MongoClient;
-const databasePassword = process.env.DB_PASSWORD;
+const { MongoClient } = require("mongodb");
+const url = process.env.DB_URL;
 const databaseName = "products_test";
 
-const url = `mongodb+srv://lightrao:${databasePassword}@cluster0.k1pcet1.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://lightrao:<password>@cluster0.k1pcet1.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-const createProduct = async (req, res, next) => {};
+  // put the object into our MongoDB database
+  const client = new MongoClient(url);
+  try {
+    await client.connect();
+    const db = client.db(databaseName);
+    await db.collection("products").insertOne(newProduct);
+  } catch (error) {
+    return res.json({ errorMessage: error });
+  }
+  client.close();
+  res.json(newProduct);
+  next();
+};
 
 const getProducts = async (req, res, next) => {};
 
