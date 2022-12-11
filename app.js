90624e4bd3f145e82b82fe8bf4384ee702fcb80a
RAO YANG <lightrao@hotmail.com>
2022-12-11 16:24:57 +0800

    Connecting to the Database & Saving the Product

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // ./app.js
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoPractice = require("./mongo");
+// const mongoPractice = require("./mongo");
+const mongoPractice = require("./mongoose");
 
 const app = express();
 
@@ -11,6 +12,6 @@ app.use(bodyParser.json());
 app.post("/products", mongoPractice.createProduct);
 
 // give me all the documents stored in a specific collection
-app.get("/products", mongoPractice.getProducts);
+// app.get("/products", mongoPractice.getProducts);
 
 app.listen(3000);
--- mongoose.js
+++ mongoose.js
@@ -1,11 +1,27 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const Product = require("./models/products");
+
+mongoose
+  .connect(process.env.DB_URL_DBNAME)
+  .then((result) => {
+    console.log("Connected to database succesfully!");
+  })
+  .catch((error) => {
+    console.log("Connection failed:", error);
+  });
 
 const createProduct = async (req, res, next) => {
   const createdProduct = new Product({
     name: req.body.name,
     price: req.body.price,
   });
+  try {
+    const result = await createdProduct.save();
+    res.json(result);
+  } catch (error) {
+    console.log("Document inserting error:", error);
+  }
 };
 
 exports.createProduct = createProduct;
